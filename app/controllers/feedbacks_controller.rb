9aaeef2c083e8fd2c5bb2d42fb1c6e1e725fee93
thejdavid <jdlavaud@gmail.com>
2015-09-23 20:15:40 +0200

    style

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,11 +1,14 @@
 class FeedbacksController < ApplicationController
   respond_to :json
   def index
+    if current_user
     z = FeedbackDetail.all
     render :json => z
+    end
   end
 
   def create
+        if current_user
     employee_id = User.find_by(email:params[:data][:name]).id
     feedback = Feedback.create!(user_id:current_user.id)
     test = params[:data].permit(:feedback_content,:motivated,:best,:recommend,:considered)
@@ -15,13 +18,20 @@ class FeedbacksController < ApplicationController
     todo.save
     render :json => nil
   end
+  end
   def search
+    if current_user
+
     user_emails = User.all.map{|user| user.email}
     render :json => user_emails
   end
+  end
   def myfb
+        if current_user
+
     user_feedbacks = FeedbackDetail.where(employee_id:current_user.id)
     render :json => user_feedbacks
+  end
   end
 
 end
