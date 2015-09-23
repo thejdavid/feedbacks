13f4b21c8a3878d3f79ef5cc8f59237ccb650c13
thejdavid <jdlavaud@gmail.com>
2015-09-23 19:02:53 +0200

    my feedback page + controller + styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :feedbacks
+  resources :feedbacks do
+    collection do
+      get 'search'
+      get 'myfb'
+    end
+  end
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- public/feedback-controller.js
+++ public/feedback-controller.js
@@ -1,5 +1,6 @@
         angular.module('FeedbackTool')
         .controller('FeedbackController',['$scope', '$http', function($scope,$http) {
+          // Feedback Index( all feedbacks)
           $http.get('/feedbacks.json').
                             then(function(response) {
                               $scope.test = " ok "
@@ -12,6 +13,7 @@
                                                             $scope.test = " error"
                             });
           $scope.submit = function() {
+            // Post a new feedback
                   $http.post('/feedbacks.json', { data:this.feedback }).
                             then(function(response) {
                   $scope.succes = 'sent';
@@ -25,7 +27,31 @@
                             });
                   // $scope.list.push(this.feedback);
               };
-          $scope.search = function() {
-            };
+              //Email suggestions for new feedback
+              $scope.selected = '';
+    $http.get('/feedbacks/search.json').
+    then(function(response) {
+      $scope.test = " ok "
+      $scope.states = response.data;
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+        $scope.test = " error"
+    });
+    $http.get('/feedbacks/myfb.json').
+                            then(function(response) {
+                              $scope.test = " ok "
+                              $scope.mylist = response.data;
+                              // this callback will be called asynchronously
+                              // when the response is available
+                            }, function(response) {
+                              // called asynchronously if an error occurs
+                              // or server returns response with an error status.
+                                                            $scope.test = " error"
+                            });
+
+
         }]);
 
