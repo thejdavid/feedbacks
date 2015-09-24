a460b8d004766e45f33cb384eabdf5ecd1008a69
thejdavid <jdlavaud@gmail.com>
2015-09-24 19:03:04 +0200

    indentation issue

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,37 +2,34 @@ class FeedbacksController < ApplicationController
   respond_to :json
   def index
     if current_user
-    z = FeedbackDetail.all
-    render :json => z
+      all_user_details = FeedbackDetail.all
+      render :json => all_user_details
     end
   end
-
   def create
-        if current_user
-    employee_id = User.find_by(email:params[:data][:name]).id
-    feedback = Feedback.create!(user_id:current_user.id)
-    test = params[:data].permit(:feedback_content,:motivated,:best,:recommend,:considered)
-    todo = FeedbackDetail.new(test)
-    todo.employee_id = employee_id
-    todo.feedback_id = feedback.id
-    todo.save
-    render :json => nil
-  end
+    if current_user
+      employee_id = User.find_by(email:params[:data][:name]).id
+      feedback = Feedback.create!(user_id:current_user.id)
+      data = params[:data].permit(:feedback_content,:motivated,:best,:recommend,:considered)
+      feedback_detail = FeedbackDetail.new(data)
+      feedback_detail.employee_id = employee_id
+      feedback_detail.feedback_id = feedback.id
+      feedback_detail.save
+      render :json => nil
+    end
   end
   def search
     if current_user
-
-    user_emails = User.all.map{|user| user.email}
-    render :json => user_emails
-  end
+      user_emails = User.all.map{|user| user.email}
+      render :json => user_emails
+    end
   end
   def myfb
-  if current_user
-    user_feedbacks = FeedbackDetail.where(employee_id:current_user.id)
-    render :json => user_feedbacks
+    if current_user
+      user_feedbacks = FeedbackDetail.where(employee_id:current_user.id)
+      render :json => user_feedbacks
+    end
   end
-  end
-
 end
 
 
