566d424c853b3f5d2b1a4789c1a4a5b8cecfb5aa
thejdavid <jdlavaud@gmail.com>
2015-09-28 19:23:04 +0200

    missing comma'

--- app/models/feedback_detail.rb
+++ app/models/feedback_detail.rb
@@ -1,6 +1,6 @@
 class FeedbackDetail < ActiveRecord::Base
   belongs_to :employee,class_name: "User"
   belongs_to :feedback
-  validates :employee_id, :feedback_content, :motivated, :best, :recommend, :considered presence: true
+  validates :employee_id, :feedback_content, :motivated, :best, :recommend, :considered, presence: true
   validates :feedback_content, length: { minimum: 250 }
 end
