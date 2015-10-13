b22b0693308ddc0f266d97d48fba1f62b8243d9f
thejdavid <jdlavaud@gmail.com>
2015-10-13 23:31:18 +0200

    redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def after_sign_up_path_for(resource)
-      root_path(resource)
+      '#/myfb'
+  end
+  def after_sign_in_path_for(resource)
+      '#/myfb'
   end
 end
